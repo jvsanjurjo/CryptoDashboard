ad63454fa31e3942cfb08b067479b04701a8c6ff
Valeriano Sanjurjo <valeriano@vsanjurjo.vsanjurjo>
2025-10-25 16:28:06 +0000

    feature: mvc template implemented

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CryptoDashboard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
